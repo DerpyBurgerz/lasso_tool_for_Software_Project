a8037629955d33a6d27875c095ef91af55c77804
DerpyBurgerz <samuel@timetoknow.nl>
2026-06-16 12:29:10 +0200

    starting on thinking about the CTR function

--- src/cumulative_triangle_routine.rs
+++ src/cumulative_triangle_routine.rs
@@ -1,4 +1,5 @@
 use bevy::math::Vec2;
+use crate::triangle_area::triangle_area;
 
 #[derive(Clone, Debug)]
 pub struct CumulativeTriangleRoutine {
@@ -22,7 +23,38 @@ impl Default for CumulativeTriangleRoutine {
 }
 
 pub fn cumulative_triangle_routine_step(points: &mut Vec<Vec2>, data: CumulativeTriangleRoutine) -> CumulativeTriangleRoutine {
-
-
+    // add input[0] to result array
     todo!()
+    // let mut result = Vec::new();
+    // result.push(input[0]);
+    //
+    //
+    // let mut first = 0;
+    // let mut middle = 1;
+    // let mut last = 2;
+    // let mut temp_area = 0f64;
+    // while last < input.len() {
+    //     // Calculate the area of the current triad
+    //     // of points
+    //     let area = triangle_area(input[first],
+    //                              input[middle],
+    //                              input[last]);
+    //     // Check If it should retain points or not
+    //     temp_area += area;
+    //     if temp_area > area_threshold {
+    //         temp_area = 0.0;
+    //         result.push(input[middle]);
+    //         //result.push(input[last]);
+    //         first = middle;
+    //         middle = first + 1;
+    //         last = first + 2;
+    //     }
+    //     else {
+    //         middle += 1;
+    //         last += 1;
+    //     }
+    // }
+    // result
+    //
+    // todo!()
 }
